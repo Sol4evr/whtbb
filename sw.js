b62384e3a94ddcef1163ac13d1fcd0be5644c1ce
Sol4evr <122612876+Sol4evr@users.noreply.github.com>
2026-09-02 15:53:55 +1000

    Bump PWA cache for repaired cloud sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "whtbb-shell-v9";
-const GAME_CACHE = "whtbb-game-v9";
+const SHELL_CACHE = "whtbb-shell-v10";
+const GAME_CACHE = "whtbb-game-v10";
 const SHELL = ["/", "/index.html", "/manifest.json", "/score-hook.js", "/category-leaderboard.js", "/icons/icon-180.png", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 self.addEventListener("install", event => {
